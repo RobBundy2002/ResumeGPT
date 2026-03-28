1eac48b634da34e2c2ae7784aa23c123750fce99
Rob <robbielbundy@gmail.com>
2026-03-27 20:43:30 -0400

    Fix Code

--- test-api.js
+++ test-api.js
@@ -2,7 +2,7 @@ const OpenAI = require('openai');
 
 // Test with your API key
 const openai = new OpenAI({
-  apiKey: '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'
+  apiKey: ''
 });
 
 async function testAPI() {
